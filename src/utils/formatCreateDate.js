216ecf45e671b134e652779fc74942e3dd944b8d
Apichet Juntodum <Apichet.jtd@outlook.com>
2023-11-21 08:54:05 +0700

    dev

--- src/utils/formatCreateDate.js
+++ src/utils/formatCreateDate.js
@@ -10,11 +10,6 @@ export const formatDate = (dates) => {
     const seconds = String(date.getUTCSeconds()).padStart(2, "0");
     const milliseconds = String(date.getUTCMilliseconds()).padStart(3, "0");
 
-    // return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}:${milliseconds}`;
-    return `${year}-${month}`;
-
-    // return `${year}-${month}-${day} `;
-
-    // return `${year}-${month}-${day}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}:${milliseconds}`;
   }
 };

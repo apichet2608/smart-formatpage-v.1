d88959e5e58558c0e630c5ec406d281aab658495
Apichet Juntodum <Apichet.jtd@outlook.com>
2023-12-06 08:45:58 +0700

    dev

---
+++ src/Components/common/Loading/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loading.css";
+
+function loading() {
+  return (
+    <div>
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default loading;
